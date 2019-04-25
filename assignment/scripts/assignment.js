4c8b9d4c0f5258c68fbca742a31efffe0ef63652
Susannah Harris <harris.susannah@gmail.com>
2019-04-25 17:29:15 -0500

    saving work

--- assignment/scripts/assignment.js
+++ assignment/scripts/assignment.js
@@ -1,15 +1,15 @@
 // REQUIRED FEATURES
 
 // 1. Create a variable with a value of an array with the following (number) values: 3, 6, 7, 10
-
+let numbers = [ 3, 6, 7, 10 ];
 // 2. Create a variable called 'myValue' with a value of the number 0
-
+let myValue = 0;
 // 3. Console log the value of the array at index 2
-
+console.log( numbers[2] );
 // 4. Remove the last item in the array, then console log the array
-
+numbers.pop();
 // 5. Add the value of number 15 to the end of the array
-
+numbers.push( 15 );
 // 6. Write a `for` loop that console logs each value in the array
 
 // STRETCH GOALS
@@ -20,5 +20,5 @@
 
 // 9. Below, rewrite the `for` loop from question 6 to a `for of` loop
 
-// 10. Reassign the value of `myValue` to 3. Then, write a `for` loop that console logs each value in the array, 
+// 10. Reassign the value of `myValue` to 3. Then, write a `for` loop that console logs each value in the array,
 // and if the index of the array matches the value of 'myValue', also console log 'Hurray!'
